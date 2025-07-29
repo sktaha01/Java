8f22bda6a2c10aae23fe7e68df80437e5cce8fb0
sktaha01 <sktaha2004@gmail.com>
2025-07-30 02:45:53 +0530

    DSA Commit

--- src/com/Taha/DSA/Array/RotatingArray.java
+++ src/com/Taha/DSA/Array/RotatingArray.java
@@ -1,5 +1,32 @@
 package com.Taha.DSA.Array;
 
+/*      [10,20,30,40,50,60,70]
+
+        after 3 rotation
+        [50,60,70,10,20,30,40]
+
+        soo
+        we have to do :
+
+        first:
+        reverse the Whole Array
+        [70,60,50,40,30,20,10]
+
+        now we can see that the  3 elements are in reverse order thats why in  second step we will reverse the first k elements
+
+        second:
+        reverse the first k elements in this case k is 3
+        thats why
+        [50,60,70,40,30,20,10]
+
+        Now We Can See the Elements Which Are After The K Elements are also reversed thats why in third step we will reverse the elements which are after the k elements
+
+        third:
+        reverse the elements after the k elements
+        thats why
+        [50,60,70,10,20,30,40]      //Final Answer
+*/
+
 import java.util.Scanner;
 
 public class RotatingArray {
